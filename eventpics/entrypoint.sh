a23b282ed184c4847cce6ad4d15cf2334cbd972b
dbrennand <52419383+dbrennand@users.noreply.github.com>
2025-01-01 13:00:05 +0000

    feat: allow for custom workers to be specified

--- eventpics/entrypoint.sh
+++ eventpics/entrypoint.sh
@@ -9,5 +9,8 @@ python manage.py collectstatic --noinput
 # Create the superuser
 python manage.py createsuperuser --noinput
 
+# Get the number of workers from the environment variable, default to 2 if not set
+WORKERS=${EVENTPICS_WSGI_WORKERS:-2}
+
 # Start the web server
-gunicorn --bind :8000 --workers 2 eventpics.wsgi
+gunicorn --bind :8000 --workers ${WORKERS} eventpics.wsgi
