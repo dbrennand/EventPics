90efba2b77bc2795e70b8a221de313aeedcc320f
dbrennand <52419383+dbrennand@users.noreply.github.com>
2025-03-01 23:02:37 +0000

    feat: add `Video` model

--- eventpics/gallery/admin.py
+++ eventpics/gallery/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Gallery, Photo
+from .models import Gallery, Photo, Video
 
 
 class GalleryAdmin(admin.ModelAdmin):
@@ -9,3 +9,4 @@ class GalleryAdmin(admin.ModelAdmin):
 
 admin.site.register(Gallery, GalleryAdmin)
 admin.site.register(Photo)
+admin.site.register(Video)
--- eventpics/gallery/models.py
+++ eventpics/gallery/models.py
@@ -1,6 +1,17 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import FileExtensionValidator
+
+
+def upload_with_id(instance, filename):
+    """
+    Saves a photo with the format <id>.<ext>
+    <id> is the UUID of the photo assigned by Django.
+    """
+    ext = filename.split(".")[-1]
+    filename = f"{instance.id}.{ext}"
+    return filename
 
 
 class Gallery(models.Model):
@@ -27,20 +38,30 @@ class Photo(models.Model):
     Photo model.
     """
 
-    def upload_with_id(instance, filename):
-        """
-        Saves a photo with the format <id>.<ext>
-        <id> is the UUID of the photo assigned by Django.
-        """
-        ext = filename.split(".")[-1]
-        filename = f"{instance.id}.{ext}"
-        return filename
-
     id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True
     )
     gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
     photo = models.ImageField(upload_to=upload_with_id)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Video(models.Model):
+    """
+    Video model.
+    """
+
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True
+    )
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
+    video = models.FileField(
+        upload_to=upload_with_id,
+        validators=[FileExtensionValidator(["MOV", "avi", "mp4", "webm", "mkv"])],
+    )
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
